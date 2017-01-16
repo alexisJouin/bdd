8c36fa0c6aec872f57b05883f4a86453555c493c
Jouin Alexis <alexis.jouin@live.fr>
2017-01-16 09:46:10 +0100

    ADD : Saisie table pour afficher son schéma TO TEST !

--- TP_BDD/src/tds/ClientInsertJDBC.java
+++ TP_BDD/src/tds/ClientInsertJDBC.java
@@ -5,6 +5,7 @@
 package tds;
 
 import java.sql.*;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,7 @@ public class ClientInsertJDBC {
     public static void main(String args[]) {
 
         try {
-            
+
             //Partie connexion BDD
             Class.forName("com.mysql.jdbc.Driver");
             Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/test?user=minty&password=greatsqldb");
@@ -32,6 +33,25 @@ public class ClientInsertJDBC {
             st.executeUpdate("INSERT INTO produit VALUES (0001, 'PC MM MMX', 5, 19.6, 6500)");
 
             ResultSet selectEx2 = st.executeQuery("SELECT reference, designation, quantite, tva, prix_ht FROM produit");
+
+            //Saisir un nom de table pour resortir son schéma
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Donner le nom de la table : ");
+            String tableSaisie = sc.nextLine();
+            System.out.println("Table saisie : " + tableSaisie);
+
+            ResultSet selectTable = st.executeQuery("SELECT * FROM " + tableSaisie);
+            ResultSetMetaData rsmd = selectTable.getMetaData();
+            int nbColonnes = rsmd.getColumnCount();
+            //On get le schéma de la table
+            for (int i = 1; i <= nbColonnes; i++) {
+                String typeCol = rsmd.getColumnTypeName(i);
+                System.out.println("TYPE colonne : " + typeCol);
+                String nomCol = rsmd.getColumnName(i);
+                System.out.println("TYPE colonne : " + nomCol);
+            }
+
+            //Afficher tout le contenu de la table
             while (selectEx2.next()) {
                 System.out.println("Référence : " + selectEx2.getInt("reference"));
                 System.out.println("Désignation : " + selectEx2.getString("designation"));
@@ -44,7 +64,7 @@ public class ClientInsertJDBC {
             connect.close();
         } catch (SQLException err) {
             System.out.println("Erreur SQL : " + err);
-        }catch(ClassNotFoundException err){
+        } catch (ClassNotFoundException err) {
             System.out.println("Erreur : " + err);
         }
 
